8f1bf6a431dbb9c3848cd92cc7fcd9b527484008
malikobansa <hackobansa@gmail.com>
2025-03-03 14:10:19 +0100

    added slideshow

--- src/page/components/Projects.js
+++ src/page/components/Projects.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import Footer from './Footer'
+import { Link } from 'react-router-dom'
 
 const Projects = () => {
   return (
     <div>
         <div className='bg-white w-full h-[50px] text-black text-center mx-auto mt-[100px] mb-[300px]'>
         <h1 className='text-2xl text-black pt-20'>We Plan, Monitor & Deliver Success</h1>
-        <button className='text-2xl mt-10 border-[2px] border-blue-300 p-5 hover:bg-orange-500 hover:text-white hover:border-gray-500'>CONTACT US</button>
+        <button className='text-2xl mt-10 border-[2px] border-blue-300 p-5 hover:bg-orange-500 hover:text-white hover:border-gray-500'>
+          <Link to="/contact">
+            CONTACT US
+          </Link>
+          </button>
       </div>
       <Footer/>
     </div>
