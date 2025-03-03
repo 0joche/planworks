e27b69db4bb52cd591f9a13f1444f60e29383762
malikobansa <hackobansa@gmail.com>
2025-03-03 14:07:22 +0100

    added slideshow

--- src/page/components/Featured.js
+++ src/page/components/Featured.js
@@ -1,16 +1,28 @@
 import React from 'react';
+import Slider from 'react-slick';
 import projectImage1 from '../../images/more/hoe.jpeg';
 import projectImage2 from '../../images/more/house.jpeg';
 import projectImage3 from '../../images/more/hot.jpeg';
 import projectImage4 from '../../images/more/home.jpeg';
-
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 const Featured = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+  };
+
   return (
     <div className='text-black border-t border-black'>
-      <h1 className='text-3xl font-bold pt-20 ml-1'>Featured Projects</h1>
-      <div className='flex flex-wrap justify-center mt-10'>
-        <div className='w-full md:w-1/3 p-2'>
+      <h1 className='text-3xl font-bold pt-20 ml-10'>Featured Projects</h1>
+      <Slider {...settings} className='mt-10'>
+        <div className='p-2'>
           <div className='overflow-hidden rounded-lg shadow-lg'>
             <img
               src={projectImage1}
@@ -19,7 +31,7 @@ const Featured = () => {
             />
           </div>
         </div>
-        <div className='w-full md:w-1/3 p-2'>
+        <div className='p-2'>
           <div className='overflow-hidden rounded-lg shadow-lg'>
             <img
               src={projectImage2}
@@ -28,7 +40,7 @@ const Featured = () => {
             />
           </div>
         </div>
-        <div className='w-full md:w-1/3 p-2'>
+        <div className='p-2'>
           <div className='overflow-hidden rounded-lg shadow-lg'>
             <img
               src={projectImage3}
@@ -37,17 +49,17 @@ const Featured = () => {
             />
           </div>
         </div>
-        <div className='w-full md:w-1/3 p-2'>
+        <div className='p-2'>
           <div className='overflow-hidden rounded-lg shadow-lg'>
             <img
               src={projectImage4}
-              alt='Project 3'
+              alt='Project 4'
               className='w-full h-auto transform transition-transform duration-500 hover:scale-110'
             />
           </div>
         </div>
-      </div>
-        <h1 className='text-3xl text-black text-center'>Project Title: KADUNA REFINERY QUICK FIX PROJECT, PROPOSED STAFF CAMP</h1>
+      </Slider>
+      <h1 className='text-3xl text-black text-center mt-10'>Project Title: KADUNA REFINERY QUICK FIX PROJECT, PROPOSED STAFF CAMP</h1>
     </div>
   );
 };
