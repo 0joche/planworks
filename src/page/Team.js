3896b4c3c1c95b039bad4916396a86df2b72bea6
Yakubu Usman <111092484+0joche@users.noreply.github.com>
2025-04-30 15:13:34 +0100

    Team.js

--- src/page/Team.js
+++ src/page/Team.js
@@ -103,11 +103,21 @@ const Team = () => {
       </div>
       
       <div className='mt-10 p-5 text-black'>
-        <p className='mb-5'>At PlanWorks Consulting Services Ltd, our team is the driving force behind every successful project we deliver.</p>
-        <p className='mb-5'>Comprising seasoned professionals with diverse backgrounds in Oil & Gas, Construction, Real Estate, and Government infrastructure, our experts bring years of combined experience in project planning, scheduling, monitoring, and control.</p>
-        <p className='mb-5'>We are planners, engineers, analysts, and strategists united by a shared commitment to precision, innovation, and results.</p>
-        <p className='mb-5'>Each member of our team is handpicked not just for technical expertise, but for a proactive mindset and a passion for helping clients meet critical project milestones—on time and within budget.</p>
-        <p className='mb-5'>At PlanWorks, we don't just manage projects—we build trust, foster collaboration, and deliver success.</p>
+        <p className='mb-5 text-xl leading-relaxed'>
+          At PlanWorks Consulting Services Ltd, our team is the driving force behind every successful project we deliver.
+        </p>
+        <p className='mb-5 text-xl leading-relaxed'>
+          Comprising seasoned professionals with diverse backgrounds in Oil & Gas, Construction, Real Estate, and Government infrastructure, our experts bring years of combined experience in project planning, scheduling, monitoring, and control.
+        </p>
+        <p className='mb-5 text-xl leading-relaxed'>
+          We are planners, engineers, analysts, and strategists united by a shared commitment to precision, innovation, and results.
+        </p>
+        <p className='mb-5 text-xl leading-relaxed'>
+          Each member of our team is handpicked not just for technical expertise, but for a proactive mindset and a passion for helping clients meet critical project milestones—on time and within budget.
+        </p>
+        <p className='mb-5 text-xl leading-relaxed'>
+          At PlanWorks, we don't just manage projects—we build trust, foster collaboration, and deliver success.
+        </p>
       </div>
 
       <Projects />
