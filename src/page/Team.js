b0414c4a5e167de0d631f6d06f40c39291ea94f3
Yakubu Usman <111092484+0joche@users.noreply.github.com>
2025-04-30 14:17:27 +0100

    Team.js

--- src/page/Team.js
+++ src/page/Team.js
@@ -3,7 +3,7 @@ import Site from '../images/teams.jpeg';
 import Logo from '../images/logo.jpeg';
 import { IoIosSearch, IoIosMenu, IoIosClose } from 'react-icons/io';
 import { Link } from 'react-router-dom';
-import { useSearch } from '../page/components/SearchContext'; // Import the useSearch hook
+import { useSearch } from '../page/components/SearchContext';
 import Projects from './components/Projects';
 
 const Team = () => {
@@ -34,10 +34,10 @@ const Team = () => {
         <main className="relative z-10">
           <ul className="ml-5 lg:ml-20 flex items-center justify-between">
             <li className="flex items-center">
-            <Link to="/" className='flex flex-row'>
+              <Link to="/" className='flex flex-row'>
                 <img src={Logo} alt='PlanWorks' className='w-[50px] h-[50px] mt-10 mr-5 rounded-3xl' />
-                <span className='text-3xl text-white mt-10'>PlanWorks Consulting </span>
-            </Link>
+                <span className='text-3xl text-white mt-10'>PlanWorks Consulting</span>
+              </Link>
             </li>
 
             {/* Hamburger Icon */}
@@ -100,15 +100,14 @@ const Team = () => {
         <p className='w-[400px] text-white ml-[20px] lg:ml-[100px]'>
           The PlanWorks team is made up of professionals of different fields, for the efficient delivery of our projects.
         </p>
-            
       </div>
-         <div className='mt-10 p-5 text-black'>
-            <p className='mb-5'>At PlanWorks Consulting Services Ltd, our team is the driving force behind every successful project we deliver.</li> 
-            <p className='mb-5'>Comprising seasoned professionals with diverse backgrounds in Oil & Gas, Construction, Real Estate, and Government infrastructure, our experts bring years of combined experience in project planning, scheduling, monitoring, and control.</li> 
-            <p className='mb-5'>We are planners, engineers, analysts, and strategists united by a shared commitment to precision, innovation, and results.</li>
-            <p className='mb-5'>Each member of our team is handpicked not just for technical expertise, but for a proactive mindset and a passion for helping clients meet critical project milestones—on time and within budget.</li>
-            <p className='mb-5'>At PlanWorks, we don’t just manage projects—we build trust, foster collaboration, and deliver success.</p>
-           </ul>
+      
+      <div className='mt-10 p-5 text-black'>
+        <p className='mb-5'>At PlanWorks Consulting Services Ltd, our team is the driving force behind every successful project we deliver.</p>
+        <p className='mb-5'>Comprising seasoned professionals with diverse backgrounds in Oil & Gas, Construction, Real Estate, and Government infrastructure, our experts bring years of combined experience in project planning, scheduling, monitoring, and control.</p>
+        <p className='mb-5'>We are planners, engineers, analysts, and strategists united by a shared commitment to precision, innovation, and results.</p>
+        <p className='mb-5'>Each member of our team is handpicked not just for technical expertise, but for a proactive mindset and a passion for helping clients meet critical project milestones—on time and within budget.</p>
+        <p className='mb-5'>At PlanWorks, we don't just manage projects—we build trust, foster collaboration, and deliver success.</p>
       </div>
 
       <Projects />
