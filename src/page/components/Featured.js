058bd6b57f42948e246780a213120c2a521b3bd2
Yakubu Usman <111092484+0joche@users.noreply.github.com>
2025-05-08 13:05:49 +0100

    Featured.js

--- src/page/components/Featured.js
+++ src/page/components/Featured.js
@@ -16,7 +16,7 @@ const Featured = () => {
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 3, // Show 3 slides at a time
+    slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 3000,
@@ -24,107 +24,116 @@ const Featured = () => {
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 2, // Show 2 slides on smaller screens
+          slidesToShow: 2,
         },
       },
       {
         breakpoint: 600,
         settings: {
-          slidesToShow: 1, // Show 1 slide on mobile screens
+          slidesToShow: 1,
         },
       },
     ],
   };
 
   return (
-    <div className='text-black border-t border-black'>
-      <h1 className='text-3xl font-bold pt-20 ml-10'>Our Projects</h1>
-      <Slider {...settings} className='mt-10'>
-        <div className='p-2'>
-          <Link to='/projects'>
-            <div className='overflow-hidden rounded-lg shadow-lg'>
+    <div className="text-black border-t border-black">
+      <h1 className="text-3xl font-bold pt-20 ml-10">Our Projects</h1>
+      
+      <Slider {...settings} className="mt-10">
+        <div className="p-2">
+          <Link to="/projects">
+            <div className="overflow-hidden rounded-lg shadow-lg">
               <img
                 src={projectImage1}
-                alt='Project 1'
-                className='w-full h-auto transform transition-transform duration-500 hover:scale-110'
+                alt="Project 1"
+                className="w-full h-auto transform transition-transform duration-500 hover:scale-110"
               />
             </div>
           </Link>
         </div>
-        <div className='p-2'>
-          <Link to='/projects'>
-            <div className='overflow-hidden rounded-lg shadow-lg'>
+        
+        <div className="p-2">
+          <Link to="/projects">
+            <div className="overflow-hidden rounded-lg shadow-lg">
               <img
                 src={projectImage2}
-                alt='Project 2'
-                className='w-full h-auto transform transition-transform duration-500 hover:scale-110'
+                alt="Project 2"
+                className="w-full h-auto transform transition-transform duration-500 hover:scale-110"
               />
             </div>
           </Link>
         </div>
-        <div className='p-2'>
-          <Link to='/projects'>
-            <div className='overflow-hidden rounded-lg shadow-lg'>
+        
+        <div className="p-2">
+          <Link to="/projects">
+            <div className="overflow-hidden rounded-lg shadow-lg">
               <img
                 src={projectImage3}
-                alt='Project 3'
-                className='w-full h-auto transform transition-transform duration-500 hover:scale-110'
+                alt="Project 3"
+                className="w-full h-auto transform transition-transform duration-500 hover:scale-110"
               />
             </div>
           </Link>
         </div>
-        <div className='p-2'>
-          <Link to='/projects'>
-            <div className='overflow-hidden rounded-lg shadow-lg'>
+        
+        <div className="p-2">
+          <Link to="/projects">
+            <div className="overflow-hidden rounded-lg shadow-lg">
               <img
                 src={projectImage4}
-                alt='Project 4'
-                className='w-full h-auto transform transition-transform duration-500 hover:scale-110'
+                alt="Project 4"
+                className="w-full h-auto transform transition-transform duration-500 hover:scale-110"
               />
             </div>
           </Link>
         </div>
       </Slider>
-      <h1 className='text-3xl text-black text-center mt-10'>
-        Proposed Staff Camp for a Refinery Project
+
+      <h1 className="text-3xl text-black text-center mt-10">
+        Staff Accommodation Camp Development – Refinery Project: Planning & Infrastructure Support
       </h1>
-      <Slider {...settings} className='mt-10'>
-        <div className='p-2'>
-          <Link to='/projects'>
-            <div className='overflow-hidden rounded-lg shadow-lg'>
+      
+      <Slider {...settings} className="mt-10">
+        <div className="p-2">
+          <Link to="/projects">
+            <div className="overflow-hidden rounded-lg shadow-lg">
               <img
                 src={projectImage5}
-                alt='Project 5'
-                className='w-full h-auto transform transition-transform duration-500 hover:scale-110'
+                alt="Project 5"
+                className="w-full h-auto transform transition-transform duration-500 hover:scale-110"
               />
             </div>
           </Link>
         </div>
-        <div className='p-2'>
-          <Link to='/projects'>
-            <div className='overflow-hidden rounded-lg shadow-lg'>
+        
+        <div className="p-2">
+          <Link to="/projects">
+            <div className="overflow-hidden rounded-lg shadow-lg">
               <img
                 src={projectImage6}
-                alt='Project 6'
-                className='w-full h-auto transform transition-transform duration-500 hover:scale-110'
+                alt="Project 6"
+                className="w-full h-auto transform transition-transform duration-500 hover:scale-110"
               />
             </div>
           </Link>
         </div>
-        <div className='p-2'>
-          <Link to='/projects'>
-            <div className='overflow-hidden rounded-lg shadow-lg'>
+        
+        <div className="p-2">
+          <Link to="/projects">
+            <div className="overflow-hidden rounded-lg shadow-lg">
               <img
                 src={projectImage7}
-                alt='Project 7'
-                className='w-full h-auto transform transition-transform duration-500 hover:scale-110'
+                alt="Project 7"
+                className="w-full h-auto transform transition-transform duration-500 hover:scale-110"
               />
             </div>
           </Link>
         </div>
       </Slider>
-      <h1 className='text-3xl text-black text-center mt-10'>
-        Planned & Monitored a Refinery Quick Fix Project
+
+      <h1 className="text-3xl text-black text-center mt-10">
+        Refinery Quick-Fix Rehabilitation Project: Planning, Monitoring & Reporting
       </h1>
     </div>
   );
