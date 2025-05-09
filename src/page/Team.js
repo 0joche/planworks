09a0449056f6f3b017b9a954e300db28f1b54bde
Yakubu Usman <111092484+0joche@users.noreply.github.com>
2025-05-09 10:05:43 +0100

    Team.js

--- src/page/Team.js
+++ src/page/Team.js
@@ -4,7 +4,7 @@ import Logo from '../images/logo.jpeg';
 import { IoIosSearch, IoIosMenu, IoIosClose } from 'react-icons/io';
 import { Link } from 'react-router-dom';
 import { useSearch } from '../page/components/SearchContext';
-import Projects from './components/Projects';
+import Footer from './components/Footer';
 
 const Team = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -122,7 +122,7 @@ const Team = () => {
         </div>
       </div>
 
-      <Projects />
+      <Footer />
     </div>
   );
 };
